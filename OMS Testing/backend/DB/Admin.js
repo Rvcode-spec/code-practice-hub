325a4886e5cdde8e63bb5e9518ad6fa5703df243
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-05-01 09:11:10 +0530

    Update

--- OMS Testing/backend/DB/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  password: { type: String }
-});
-
-module.exports = mongoose.model("Admin", adminSchema);
--- OMS Testing/backend/DB/config.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-if(mongoose.connect('mongodb://localhost:27017/OMS')){
-    console.log("mongoDB Connect");
-    }
